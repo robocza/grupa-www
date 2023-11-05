e26e9e510d7bf55b2ad6bbb04f5ff257282e1963
cezarystepkowski <cezary.stepkowski@protonmail.com>
2023-11-05 17:12:27 +0100

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://grupa.robocza.org",
     integrations: [
         tailwind(),
-        image({
-            serviceEntryPoint: '@astrojs/image/sharp'
-        }),
     ],
     vite: {
         server: {
