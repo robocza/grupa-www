b7c3370da26bdafefb75275d00bff170a4c82b75
cezarystepkowski <cezary.stepkowski@protonmail.com>
2024-06-30 20:26:28 +0200

    Disable sharp image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://grupa.robocza.org",
+    image: {
+        service: passthroughImageService()
+    },
     integrations: [
         tailwind(),
     ],
